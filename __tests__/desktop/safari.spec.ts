75d1a1043f94ee30861347e7470c3cfe66443843
Nikita Almanov <131481562+nikkeyl@users.noreply.github.com>
2024-01-09 08:35:53 +0300

    refactor(desktop-safari): remove backticks (#9)

--- __tests__/desktop/safari.spec.ts
+++ __tests__/desktop/safari.spec.ts
@@ -2,7 +2,7 @@ import { webkit } from 'playwright';
 import { test } from '@playwright/test';
 
 // https://playwright.dev/docs/writing-tests
-test(`Test Web App on Desktop Safari`, async () => {
+test('Test Web App on Desktop Safari', async () => {
   const browser = await webkit.launch();
   const page = await browser.newPage();
 
