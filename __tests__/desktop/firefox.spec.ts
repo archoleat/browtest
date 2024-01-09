6dc9f21a5bd9f0078e06b35f06cd0921ff6cd69d
Nikita Almanov <131481562+nikkeyl@users.noreply.github.com>
2024-01-09 03:00:02 +0300

    fix(tests): remove `.toHaveURL` method

--- __tests__/desktop/firefox.spec.ts
+++ __tests__/desktop/firefox.spec.ts
@@ -1,14 +1,11 @@
 import { firefox } from 'playwright';
-import { expect, test } from '@playwright/test';
-
-import settings from '../../core/settings.ts';
+import { test } from '@playwright/test';
 
 // https://playwright.dev/docs/writing-tests
 test('Test Web App on Desktop Firefox', async () => {
   const browser = await firefox.launch();
   const page = await browser.newPage();
 
-  await expect(page).toHaveURL(settings.url);
   await page.goto('/');
   await page.pause();
 });
