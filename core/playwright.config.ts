2fa88a07e91ecab9348e7ea0341c3ee314bf116c
Nikita Almanov <131481562+nikkeyl@users.noreply.github.com>
2024-01-09 10:33:50 +0300

    ci: add `isCI` to headless (#14)

--- core/playwright.config.ts
+++ core/playwright.config.ts
@@ -1,17 +1,19 @@
 import { defineConfig } from '@playwright/test';
 
+const isCI = process.env['CI'];
+
 // https://playwright.dev/docs/test-configuration
 export default defineConfig({
   fullyParallel: true,
   reporter: 'html',
   testDir: '../__tests__',
-  workers: process.env['CI'] ? 1 : 3,
+  workers: isCI ? 1 : 3,
   expect: { timeout: 30000 },
   use: {
     baseURL: 'http://localhost:3000/',
     bypassCSP: true,
     colorScheme: 'dark',
-    headless: false,
+    headless: !!isCI,
     trace: 'on-first-retry',
   },
 });
