c511cd27d90198b42a18ad0cb905aef605bc26ad
Nikita Almanov <131481562+nikkeyl@users.noreply.github.com>
2024-01-15 09:27:30 +0300

    fix(tests): remove `describe`

--- __tests__/desktop/safari.spec.ts
+++ __tests__/desktop/safari.spec.ts
@@ -5,14 +5,12 @@ import { test } from '@playwright/test';
 import settings from '../../core/settings';
 
 // https://playwright.dev/docs/writing-tests
-describe('Desktop Safari', () => {
-  test('Test', async () => {
-    const browser = await webkit.launch({
-      devtools: settings.devTools,
-    });
-    const page = await browser.newPage();
-
-    await page.goto(settings.rootFolder);
-    await page.pause();
+test('Desktop Safari', async () => {
+  const browser = await webkit.launch({
+    devtools: settings.devTools,
   });
+  const page = await browser.newPage();
+
+  await page.goto(settings.rootFolder);
+  await page.pause();
 });
--- __tests__/mobile/chrome.spec.ts
+++ __tests__/mobile/chrome.spec.ts
@@ -5,17 +5,15 @@ import { test } from '@playwright/test';
 import settings from '../../core/settings';
 
 // https://playwright.dev/docs/writing-tests
-describe('Mobile Chrome', () => {
-  test('Test', async () => {
-    const browser = await chromium.launch({
-      devtools: settings.devTools,
-    });
-    const context = await browser.newContext({
-      ...devices['Pixel 7'],
-    });
-    const page = await context.newPage();
-
-    await page.goto(settings.rootFolder);
-    await page.pause();
+test('Mobile Chrome', async () => {
+  const browser = await chromium.launch({
+    devtools: settings.devTools,
   });
+  const context = await browser.newContext({
+    ...devices['Pixel 7'],
+  });
+  const page = await context.newPage();
+
+  await page.goto(settings.rootFolder);
+  await page.pause();
 });
